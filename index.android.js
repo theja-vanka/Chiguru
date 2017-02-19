4ee3d330c584aeb9426d6bf5c06a8bf0ac5e4d17
theja-vanka <theja.vanka@gmail.com>
2017-02-19 12:24:09 +0530

    Rotate splash p1

--- index.android.js
+++ index.android.js
@@ -9,26 +9,64 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Animated,
+  Easing
 } from 'react-native';
 
+class BackgroundImage extends Component {
+  
+    render() {
+        return (
+            <Image source={require('./images/splash_fin.png')}
+                  style={styles.backgroundImage}>
+
+                  {this.props.children}
+
+            </Image>
+        );
+    }
+}
+
+
 export default class Chiguru extends Component {
+  constructor () {
+  super()
+  this.spinValue = new Animated.Value(0)
+}
   render() {
+     const spin = this.spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  })
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <BackgroundImage>
+        <Animated.Image
+        style={{
+          flex: 1,
+          width: null,
+          height: null,
+          resizeMode: 'cover',
+          transform: [{rotate: spin}] }}
+          source={require('./images/sp_lines.png')} />
+      </BackgroundImage>
     );
   }
+  componentDidMount () {
+  this.spin()
+}
+spin () {
+  this.spinValue.setValue(0)
+  Animated.timing(
+    this.spinValue,
+    {
+      toValue: 1,
+      duration: 40000,
+      easing: Easing.linear
+    }
+  ).start(() => this.spin())
+}
 }
 
 const styles = StyleSheet.create({
@@ -36,8 +74,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'rgba(0,0,0,0)',
   },
+  backgroundImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
+    },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
