5727a62526ee84895e3d3aed93b93e1cfb0ba946
theja-vanka <theja.vanka@gmail.com>
2017-02-25 22:09:46 +0530

    Abts

--- Aboutus.js
+++ Aboutus.js
@@ -1,6 +1,32 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Navigator, StyleSheet, TouchableWithoutFeedback,BackAndroid } from 'react-native';
+import { View,Image, Navigator, StyleSheet, BackAndroid } from 'react-native';
 import {Text} from 'react-native-elements';
+
+
+class BackgroundImage extends Component {
+  
+    render() {
+        return (
+            <Image source={require('./images/camb.png')}
+                  style={styles.backgroundImage}>
+
+   {this.props.children}
+            </Image>
+        );
+    }
+}
+class BackgroundLogo extends Component {
+  
+    render() {
+        return (
+            <Image source={require('./images/abtcig.png')}
+                  style={styles.backgroundImage}>
+
+   {this.props.children}
+            </Image>
+        );
+    }
+}
 
 export default class Aboutus extends Component {
   static get defaultProps() {
@@ -19,17 +45,20 @@ export default class Aboutus extends Component {
 
   render() {
     return (
-<View style={styles.container}>
-  <Text style={styles.welcome}>About Us</Text>
-<Text style={styles.points}>Chiguru - An Intercollegiate Mega Cultural Extravaganza of Cambridge Institute of Technology, Bengaluru.</Text>
+      <View style={styles.container}>
+<View style={styles.container1}>
+  <BackgroundImage />
+  </View>
+  <View style={styles.container2}>
+    <Text style={styles.welcome}>About Us</Text>
+    <View>
+    <Text style={styles.points}>Chiguru - An Intercollegiate Mega Cultural Extravaganza of Cambridge Institute of Technology, Bengaluru.</Text>
 <Text style={styles.points}>The National Level Cultural Extravaganza of Cambridge Institute of Technology, Bengaluru is back again on this March 11th and 12th,2017 .Mark the dates and “GET CONNECTED “ with us.....</Text>
 <Text style={styles.points}>IT'S BACK!! IT'S BETTER!! IT'S BIG!!</Text>
 <Text style={styles.points}> Compete against the most talented from all around the city and set yourself apart. We at CITech provide the best opportunity for the MAJESTIC participants to flaunt what they've got.</Text>
 <Text style={styles.points}>SURPRISE...SUSPENSE..and... ACTION...</Text>
-
-
-
-
+  </View>
+  </View>
   </View>
     )
   }
@@ -41,21 +70,33 @@ export default class Aboutus extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#100F20',
+    backgroundColor: 'white',
+  },
+    container1: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0)',
+  },
+    container2: {
+    flex: 2,
+    backgroundColor: 'rgba(0,0,0,0)',
   },
   welcome: {
-    fontSize: 30,
+    fontSize: 20,
     textAlign: 'center',
     margin: 10,
-    color: 'white',
+    color: '#0f0e20',
   },
     points: {
-    fontSize: 15,
+    fontSize: 12,
     textAlign: 'center',
     margin: 10,
-    color: 'white',
+    color: '#0f0e20',
   },
+   backgroundImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
+    },
 
 });
--- location.js
+++ location.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text, Navigator, TouchableWithoutFeedback,BackAndroid } from 'react-native';
+import { View, Text, Navigator, WebView, BackAndroid } from 'react-native';
 
 export default class Location extends Component {
   static get defaultProps() {
@@ -18,11 +18,9 @@ export default class Location extends Component {
 
   render() {
     return (
-      <TouchableWithoutFeedback>
-        <View>
-          <Text onPress={this.goBack}>Location</Text>
-        </View>
-      </TouchableWithoutFeedback>
+      <WebView
+        source={{uri: 'https://www.google.co.in/maps/place/Cambridge+Institute+of+Technology/@13.0123067,77.7041594,15z/data=!4m12!1m6!3m5!1s0x0:0xf8f534df379561ed!2sCambridge+Institute+of+Technology!8m2!3d13.0123067!4d77.7041594!3m4!1s0x0:0xf8f534df379561ed!8m2!3d13.0123067!4d77.7041594'}}
+      />
     )
   }
   componentDidMount() {
